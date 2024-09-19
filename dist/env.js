1e24dc70fb7c3ce745a0b87894565a4d0358ff60
GustavoFSProg <gustavo.prog40@gmail.com>
2024-09-19 19:04:23 -0300

    deplopy onining

--- dist/env.js
+++
@@ -1,7 +0,0 @@
-"use strict";Object.defineProperty(exports, "__esModule", {value: true});var _zod = require('zod');
-
-const envSchema = _zod.z.object({
-  DATABASE_URL: _zod.z.string().url(),
-})
-
- const env = envSchema.parse(process.env.DATABASE_URL); exports.env = env
