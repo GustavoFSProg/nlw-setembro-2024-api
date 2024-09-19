b8e196097542b66847b56af57bc8bbc89f8929e5
GustavoFSProg <gustavo.prog40@gmail.com>
2024-09-19 16:40:43 -0300

    deploy

---
+++ server.ts
@@ -0,0 +1,38 @@
+import fastify from 'fastify'
+import fastifyCors from '@fastify/cors'
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+import { createGoalRoute } from './src/http/routes/create-goal'
+import { createGoalCompletionRoute } from './src/http/routes/create-goal-completion'
+import { getWeekSummaryRoute } from './src/http/routes/get-week-summary'
+import { getWeekPendingGoalsRoute } from './src/http/routes/get-week-pending-goals'
+import { viewGoals } from './src/http/routes/get-goals'
+// import { createGoalRoute } from './routes/create-goal'
+// import { createGoalCompletionRoute } from './routes/create-goal-completion'
+// import { getWeekSummaryRoute } from './routes/get-week-summary'
+// import { getWeekPendingGoalsRoute } from './routes/get-week-pending-goals'
+// import { viewGoals } from './routes/get-goals'
+
+const { PORT } = process.env
+
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.register(fastifyCors, { origin: '*' })
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.register(createGoalRoute)
+app.register(createGoalCompletionRoute)
+app.register(getWeekSummaryRoute)
+app.register(getWeekPendingGoalsRoute)
+app.register(viewGoals)
+
+app.listen({ port: PORT }).then(() => {
+  console.log(`💪 HTTP server running!: ${PORT}`)
+})
+
+export default app
