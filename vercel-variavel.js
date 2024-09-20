4ccc38367eb68fa77ebb1c97d8b8921ca8b374ab
GustavoFSProg <gustavo.prog40@gmail.com>
2024-09-20 10:27:58 -0300

    rotas deploy

--- vercel-variavel.js
+++
@@ -1,8 +0,0 @@
-
-DATABASE_URL	postgresql://poxketdb_owner:******@ep-aged-union-a5isuxtp-pooler.us-east-2.aws.neon.tech/poxketdb?sslmode=require
-DATABASE_URL_UNPOOLED	postgresql://poxketdb_owner:******@ep-aged-union-a5isuxtp.us-east-2.aws.neon.tech/poxketdb?sslmode=require
-
-
-DATABASE_URL	postgresql://poxketdb_owner:******@ep-aged-union-a5isuxtp-pooler.us-east-2.aws.neon.tech/poxketdb?sslmode=require
-
-postgresql://poxketdb_owner:******@ep-aged-union-a5isuxtp.us-east-2.aws.neon.tech/poxketdb?sslmode=require
